04a88e8bd8349afd9cc34dff7b2a03e80012bc3e
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-09-11 13:31:28 +0530

    BuiltInFind_Solution.cpp

--- String_Algorithms/Implement_strStr/BuiltInFind_Solution.cpp
+++ String_Algorithms/Implement_strStr/BuiltInFind_Solution.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        auto pos = haystack.find(needle);
+        if (pos == string::npos) {  // Use string::npos instead of -1
+            return -1;
+        }
+        return (int)pos;
+    }
+};
