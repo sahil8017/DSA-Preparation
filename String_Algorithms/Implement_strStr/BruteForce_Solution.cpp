3a9853db8a906da1c62a97c36fc669e2d115c376
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-09-11 13:29:55 +0530

    BruteForce_Solution.cpp

--- String_Algorithms/Implement_strStr/BruteForce_Solution.cpp
+++ String_Algorithms/Implement_strStr/BruteForce_Solution.cpp
@@ -1,1 +1,28 @@
 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length(), m = needle.length();
+
+        // If needle is an empty string, return 0
+        if (m == 0)
+            return 0;
+
+        // Loop through haystack to check for matching substring
+        for (int i = 0; i <= n - m; i++) { // Include n-m position
+            bool found = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found) {
+                return i; // Return the index of the first occurrence
+            }
+        }
+
+        return -1; // Return -1 if no match is found
+    }
+};
