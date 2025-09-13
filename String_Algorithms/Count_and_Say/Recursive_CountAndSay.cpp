9b8cd7596db427ed0516e6938e08550d23c437f8
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-09-13 15:45:07 +0530

    Recursive_CountAndSay.cpp

--- String_Algorithms/Count_and_Say/Recursive_CountAndSay.cpp
+++ String_Algorithms/Count_and_Say/Recursive_CountAndSay.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+string count(string s){
+    string result = "";
+    for(int i = 0; i < s.length(); i++){
+        int count = 1;
+        while(i + 1 < s.length() && s[i] == s[i + 1]){
+            count++;
+            i++;
+        }
+        result += to_string(count) + s[i];
+    }
+    return result;
+}
+string countAndSay(int n){
+    if(n == 1) return "1";
+    return count(countAndSay(n-1));
+}
+
+int main(){
+    int n = 4;
+    cout << countAndSay(n) << endl;
+}
