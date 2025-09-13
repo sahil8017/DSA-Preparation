eb740708732a79bdffe56ef80a92d7c5e6e8553a
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-09-13 15:38:13 +0530

    Iterative_CountAndSay.cpp

--- String_Algorithms/Count_and_Say/Iterative_CountAndSay.cpp
+++ String_Algorithms/Count_and_Say/Iterative_CountAndSay.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 string countAndSay(int n){
+    if(n == 1) return "1";
     string result = "1";
     for(int i = 1; i < n; i++){
         int count = 1;
