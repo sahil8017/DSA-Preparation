e90eeba75276e7c9991c30229618d9e843c61834
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-09-13 15:36:58 +0530

    Iterative_CountAndSay.cpp

--- String_Algorithms/Count_and_Say/Iterative_CountAndSay.cpp
+++ String_Algorithms/Count_and_Say/Iterative_CountAndSay.cpp
@@ -1,1 +1,31 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+string countAndSay(int n){
+    string result = "1";
+    for(int i = 1; i < n; i++){
+        int count = 1;
+        char curcount = result[0];
+        string temp = "";
+        for(int j = 1; j < result.length(); j++){
+            if(result[j] == curcount){
+                count++;
+            }else{
+                temp += to_string(count);
+                temp.push_back(curcount);
+                curcount = result[j];
+                count = 1;
+            }
+        }
+        temp += to_string(count);
+        temp.push_back(curcount);
+        
+        result.swap(temp);
+    }
+    return result;
+}
+
+int main(){
+    int n = 4;
+    cout << countAndSay(n) << endl;
+}
