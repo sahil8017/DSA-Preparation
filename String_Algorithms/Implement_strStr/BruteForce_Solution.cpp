409e6d7bc778a1ba5af0eed194bad8454bad78e0
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-09-13 10:43:32 +0530

    BruteForce_Solution.cpp

--- String_Algorithms/Implement_strStr/BruteForce_Solution.cpp
+++ String_Algorithms/Implement_strStr/BruteForce_Solution.cpp
@@ -1,28 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        int n = haystack.length(), m = needle.length();
-
-        // If needle is an empty string, return 0
-        if (m == 0)
-            return 0;
-
-        // Loop through haystack to check for matching substring
-        for (int i = 0; i <= n - m; i++) { // Include n-m position
-            bool found = true;
-            for (int j = 0; j < m; j++) {
-                if (haystack[i + j] != needle[j]) {
-                    found = false;
-                    break;
-                }
+int strStr(string haystack, string needle){
+    int h = haystack.length(), n = needle.length();
+    
+    if(n == 0) return 0;
+    
+    for(int i = 0; i < h - n; i++){
+        bool found = true;
+        for(int j = 0; j < n; j++){
+            if(haystack[i+j] != needle[j]){
+                found = false;
+                break;
             }
-
-            if (found) {
-                return i; // Return the index of the first occurrence
+            
+            if(found){
+                return i;
             }
         }
-
-        return -1; // Return -1 if no match is found
     }
-};
+    return -1;
+}
+int main(){
+    
+    string haystack = "sadbutsad";
+    string needle = "but";
+    cout <<"needle found at position i = "<< strStr(haystack, needle) << endl;
+}
