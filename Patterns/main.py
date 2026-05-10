3815b31944e798ccd27018212e5328e72e8aa2c9
Sahil Gupta <sahilgupta4oct@gmail.com>
2026-05-11 02:39:16 +0530

    Patterns Solved

--- Patterns/main.py
+++ Patterns/main.py
@@ -49,7 +49,7 @@ def pattern5(n):
         for j in range(1,i+1):
             print("* ",end="")
         print()    
-    for i in range(n,0,-1):        
+    for i in range(n-1,0,-1):        
         for j in range(1,i+1):
             print("* ",end="")
         print()
